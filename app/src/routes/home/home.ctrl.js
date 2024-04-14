995cc56637917bcbc6fcad9c06425c8839777786
pooky1004 <hounjoung@tsnlab.com>
2024-04-14 23:24:34 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,44 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const { use } = require(".");
+
+const users = {
+    id: ["pooky", "pooky1004", "rhjoung"],
+    psword: ["i5lovepooky!@#$%", "1234", "i0loveme^("],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    
+    login: (req, res) => {
+        res.render("home/login");
+    },    
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+    },
 };
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,   
 };
