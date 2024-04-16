7f966e2ad0ef4af511aab71f50679b632aa4b229
pooky1004 <hounjoung@tsnlab.com>
2024-04-16 23:10:27 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -11,9 +11,12 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if (!id.value) return alert("아이디를 입력해 주십시요.");
+    if (psword.value !== confirmPsword.value) return alert("비밀번호가 일치하지 않습니다.");
+    
     const req = {
         id: id.value,
-        namw: name.value,
+        name: name.value,
         psword: psword.value,
         confirmPsword: confirmPsword.value,
     };
