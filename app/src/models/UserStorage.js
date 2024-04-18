939a3e757dc46f102dcc9071922947876053a430
pooky1004 <hounjoung@tsnlab.com>
2024-04-18 21:31:18 +0900

    fs 모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -16,15 +16,55 @@ class UserStorage {
 
     //     return userInfo;
     // }
+
+    // static #getUsers(data, fields) {
+    //     const users = JSON.parse(data);
+    //     if(isAll) return users;
+
+    //     const newUsers = fields.reduce((newUsers, field) => {
+    //         if (users.hasOwnProperty(field)) {
+    //             newUsers[field] = users[field];
+    //         }
+    //         return newUsers;            
+    //     }, {});
+    //     return newUsers;
+    // }
+
+    static getUsers(isAll, ...fields) {
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            const users = JSON.parse(data);
+            if(isAll) return users;
+            
+            const newUsers = fields.reduce((newUsers, field) => {
+                if (users.hasOwnProperty(field)) {
+                    newUsers[field] = users[field];
+                }
+                return newUsers;            
+            }, {});
+            return newUsers;            
+            // return this.#getUsers(data, isAll, fields);
+            // return this.#getUserInfo(data, id);
+            // const users = JSON.parse(data);
+            // const idx = users.id.indexOf(id);
+            // const usersKeys = Object.keys(users);
+            // const userInfo = usersKeys.reduce((newUser, info) => {
+            //     newUser[info] = users[info][idx];
+            //     return newUser;
+            // }, {});
     
-    static getUsers(...fields) {
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
+            // return userInfo;          
+        })
+        .catch(console.error);
+
+        // const newUsers = fields.reduce((newUsers, field) => {
+        //     if (users.hasOwnProperty(field)) {
+        //         newUsers[field] = users[field];
+        //     }
+        //     return newUsers;
+        // }, {});
+        // return newUsers;
     }
 
     static getUserInfo(id) {
@@ -45,12 +85,30 @@ class UserStorage {
             .catch(console.error);
     }
 
-    static save(userInfo) {
-        // const users = this.#users;
+    static async save(userInfo) {
+        // const users = await this.getUsers("id", "psword", "name");
+        const users = await this.getUsers(true);
+
+        if(users.id.includes(userInfo.id)) {
+            throw  '이미 존재하는 아이디 입니다.';
+        }
+
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        return { success: true };
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+        return {success: true};
+
+
+        // console.log(users);
+
+        
+
+        // const users = this.#users;
+        // users.id.push(userInfo.id);
+        // users.name.push(userInfo.name);
+        // users.psword.push(userInfo.psword);
+        // return { success: true };
     }
 }
 
